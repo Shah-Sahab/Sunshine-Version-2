5dd866956ac24b7eb7dff8656fb8aa25f690d0e2
Syed Ahmed Hussain <ahmedhussaindev@gmail.com>
2017-04-19 00:29:36 -0500

    Altering layout 6.20:
    Transition Animations and Target Animations

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -18,6 +18,8 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -139,7 +141,9 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         } else {
             Intent intent = new Intent(this, DetailActivity.class)
                             .setData(contentUri);
-            startActivity(intent);
+//            startActivity(intent);
+            ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
+            ActivityCompat.startActivity(this, intent, activityOptionsCompat.toBundle());
         }
     }
 
